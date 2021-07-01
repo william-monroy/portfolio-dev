a1eb0a239ba52b98b501b3baa651b46068c5b4e6
William Monroy <A00829796@itesm.mx>
2021-07-01 18:56:35 -0500

    portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,91 @@
+import PortfolioList from '../portfolioList/PortfolioList';
 import './portfolio.scss';
+import {useState, useEffect} from 'react';
+import {
+    featuredPortfolio,
+    webPortfolio,
+    mobilePortfolio,
+    desktopPortfolio,
+    contentPortfolio
+} from '../../data'
 
 const Portfolio = () => {
+
+    const [selected, setSelected] = useState("featured")
+    const [data, setData] = useState([])
+
+    const list = [
+        {
+            id: 'featured',
+            title: 'Featured',
+        },
+        {
+            id: 'web',
+            title: 'Web App',
+        },
+        {
+            id: 'mobile',
+            title: 'Mobile App',
+        },
+        {
+            id: 'desktop',
+            title: 'Desktop App',
+        },
+        {
+            id: 'content',
+            title: 'Content',
+        },
+    ];
+
+    useEffect(() => {
+        
+        switch (selected) {
+            case "featured":
+                setData(featuredPortfolio)
+                break;
+            case "web":
+                setData(webPortfolio)
+                break;
+            case "mobile":
+                setData(mobilePortfolio)
+                break;
+            case "desktop":
+                setData(desktopPortfolio)
+                break;
+            case "content":
+                setData(contentPortfolio)
+                break;
+            default:
+                setData(featuredPortfolio)
+                break;
+        }
+
+    }, [selected]);
+
     return (
         <div className="portfolio" id="portfolio">
-            
+            <h1>Potfolio</h1>
+            <ul>
+                {list.map(item=>(
+                    <PortfolioList 
+                        title={item.title} 
+                        active={selected === item.id}
+                        setSelected={setSelected}
+                        id={item.id}
+                    />
+                ))}      
+            </ul>
+            <div className="container">
+                {data.map((d)=>(
+                    <div className="item">
+                        <img 
+                            src={d.img} 
+                            alt={d.title} 
+                        />
+                        <h3>{d.title}</h3>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
---
+++ src/data.js
@@ -0,0 +1,98 @@
+export const featuredPortfolio = [{
+        id: 1,
+        title: "Nebula",
+        img: "https://i.postimg.cc/kGtRg9wZ/Nebula.png",
+    },
+    {
+        id: 2,
+        title: "GameJam",
+        img: "https://i.postimg.cc/mgTLqH6P/game-jam.png",
+    },
+    {
+        id: 3,
+        title: "Newsletter",
+        img: "https://i.postimg.cc/zDRkB1L8/Newsletter.png",
+    },
+    {
+        id: 4,
+        title: "MLogic2D",
+        img: "https://i.postimg.cc/MTLjwX0V/MLogic2D.png",
+    },
+    {
+        id: 5,
+        title: "Workshop HackLATAM",
+        img: "https://i.postimg.cc/wMVMvvLw/Microsoft-HACKLATAM.png",
+    },
+    {
+        id: 6,
+        title: "PISA-Games",
+        img: "https://i.postimg.cc/J4V5HdJJ/PISA-Games.png",
+    },
+    {
+        id: 7,
+        title: "CONNEQ",
+        img: "https://i.postimg.cc/j52JByfR/CONNEQ.png",
+    },
+];
+
+export const webPortfolio = [
+    {
+        id: 1,
+        title: "Nebula",
+        img: "https://i.postimg.cc/kGtRg9wZ/Nebula.png",
+    },
+    {
+        id: 2,
+        title: "GameJam",
+        img: "https://i.postimg.cc/mgTLqH6P/game-jam.png",
+    },
+    {
+        id: 3,
+        title: "Newsletter",
+        img: "https://i.postimg.cc/zDRkB1L8/Newsletter.png",
+    }
+];
+
+export const mobilePortfolio = [
+    {
+        id: 1,
+        title: "CONNEQ",
+        img: "https://i.postimg.cc/j52JByfR/CONNEQ.png",
+    }
+];
+
+export const desktopPortfolio = [
+    {
+        id: 1,
+        title: "MLogic2D",
+        img: "https://i.postimg.cc/MTLjwX0V/MLogic2D.png",
+    },
+    {
+        id: 2,
+        title: "PISA-Games",
+        img: "https://i.postimg.cc/J4V5HdJJ/PISA-Games.png",
+    },
+    {
+        id: 3,
+        title: "SAVE",
+        img: "https://i.postimg.cc/c4RMRMMz/SAVE.png",
+    },
+    {
+        id: 4,
+        title: "SisVentas",
+        img: "https://i.postimg.cc/7ZX3cHRM/Sis-Ventas.png",
+    },
+    {
+        id: 5,
+        title: "Translator",
+        img: "https://i.postimg.cc/N0M6whqh/Translator.png",
+    },
+];
+
+export const contentPortfolio = [
+    {
+        id: 1,
+        title: "Workshop HackLATAM",
+        img: "https://i.postimg.cc/wMVMvvLw/Microsoft-HACKLATAM.png",
+    },
+];
